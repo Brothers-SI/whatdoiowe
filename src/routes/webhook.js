3c55d89181689837e938ae3ed0b01d6a7702bb08
Douglas Haubert <dhaubert.ti@gmail.com>
2020-05-19 01:13:41 -0300

    telegram: added support to telegram using new library via webhook

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -1,10 +1,7 @@
 const { Router } = require("express");
 
 const router = Router();
-router.post(`/telegram/${process.env.TELEGRAM_HASH}`, (req, res) => {
-  const {message: from, chat, date, text, entities } = req.body;
-  console.log(req.body.message);
-  res.send(req.body)
-})
+const Telegram = require('../controllers/http/telegram')
+router.post(`/telegram/${process.env.TELEGRAM_HASH}`, Telegram.receive);
 
 module.exports = router;
